48415c96eccb8d20dadaa0fa3bdda34da2870602
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2023-01-24 15:13:39 -0800

    Add files via upload

---
+++ what.py
@@ -0,0 +1,60 @@
+"""
+Author: kjfreidhof
+
+file: what.py 
+
+about: This is a fun python script that i designed and that im going to keep adding to as 
+time goes on with bugs bunny one of my favorite cartoon characters ment as a fun learning program 
+to showcase how varriables work 
+
+
+
+
+"""
+# These are str Varriables which is a varriable contained in a string of text   
+
+Bugs = "Whats up doc?"
+whelmer = "Im Hunting wabbits"
+
+# This here prints out the text 
+print(Bugs)
+print(whelmer)
+
+# The Varriables 
+Bugs2 = "Chews off carrot with evil grin"
+Bugs3 = "What kinda Wabbits"
+
+whelmer2 = "Not just any Wabbit"
+
+# printing out the varriable 
+print(Bugs2)
+print(Bugs3)
+print(whelmer2)
+
+# varriables
+Bugs4 = "Mmm say does he have Big teeth Big ears"
+Bugs5 = "Says Whats up Doc?"
+
+whelmer3 = "Yeah thats it"
+
+Bugs6 = "Sorry havent seen him"
+
+# printing out the varriables
+print(Bugs4)
+print(Bugs5)
+print(whelmer3)
+print(Bugs6)
+
+
+# varriables
+whelmer4 = "Oh wait a minute?"
+whelmer5 = "Blam Blam!!!"
+Pig = "Thats all Folks!!!"
+
+# printing out the varriables
+print(whelmer4)
+
+print(whelmer5)
+
+print(Pig)
+
